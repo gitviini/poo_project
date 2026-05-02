cd581464c3637e1c69fe0514eb190624053eec32
Leandro Manuel Francisco <leandro_00010@outlook.com>
2026-05-01 22:45:24 -0300

    implemented MailTrap to send password recovery and a test for its service method

---
+++ app/src/test/java/com/arena/app/testController/EmailServiceTest.java
@@ -0,0 +1,21 @@
+package com.arena.app.testController;
+
+import com.arena.app.service.EmailService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Scanner;
+
+@SpringBootTest
+public class EmailTestingController {
+    @Autowired
+    private EmailService emailService;
+    @Test
+    public void sendEmailTest() {
+        var scanner = new Scanner(System.in);
+        System.out.println("Insira o seu endereço de email: ");
+        var email = scanner.nextLine();
+        emailService.sendEmail(email,"Testando envio de email de recuperação", "Olá, este é um email de teste para verificar o funcionamento do serviço de envio de emails. Se você recebeu este email, significa que o serviço está funcionando corretamente.");
+    }
+}
