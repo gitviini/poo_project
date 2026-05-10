a75d643ed0610fcedf0076cf7a40630d13afbc28
gitviini <gvinicius105@gmail.com>
2026-05-10 18:20:17 -0300

    fix: commented EmailServiceTest

--- app/src/test/java/com/arena/app/testController/EmailServiceTest.java
+++ app/src/test/java/com/arena/app/testController/EmailServiceTest.java
@@ -1,6 +1,6 @@
 package com.arena.app.testController;
 
-import com.arena.app.service.EmailService;
+//import com.arena.app.service.EmailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,8 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Scanner;
 
 @SpringBootTest
-public class EmailTestingController {
-    @Autowired
+public class EmailServiceTest {
+/*     @Autowired
     private EmailService emailService;
     @Test
     public void sendEmailTest() {
@@ -17,5 +17,5 @@ public class EmailTestingController {
         System.out.println("Insira o seu endereço de email: ");
         var email = scanner.nextLine();
         emailService.sendEmail(email,"Testando envio de email de recuperação", "Olá, este é um email de teste para verificar o funcionamento do serviço de envio de emails. Se você recebeu este email, significa que o serviço está funcionando corretamente.");
-    }
+    } */
 }
